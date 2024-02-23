2a267db291e85277cc633893add99f65099c036e
Franky <nikto8748@gmail.com>
2024-02-23 19:57:00 +0300

    Second task of the problem was done

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -1,7 +1,27 @@
-import sys, glob
+import sys, glob, zlib
+from os.path import basename, dirname
+
+SHIFT = "   "   
 
 match len(sys.argv):
     case 2:
         path = sys.argv[1]
         for i in glob.iglob(path + "/.git/refs/heads/*"):
-            print(i[i.rfind("/") + 1:])
+            print(basename(i))
+
+    case 3:
+        path, branch = sys.argv[1:]
+        for i in glob.iglob(path + "/.git/refs/heads/*"):
+            if branch == basename(i):
+                with open(i, "r") as f:
+                    head_id = f.read().split()[0]
+                for store in glob.iglob(path + "/.git/objects/??/*"):
+                    if basename(dirname(store)) + basename(store) == head_id:
+                        with open(store, "rb") as f:
+                            obj = zlib.decompress(f.read())
+                            header, _, body = obj.partition(b'\x00')
+                            kind, size = header.split()
+                            out = body.decode().replace('\n', '\n' + SHIFT)
+                            print(f"{SHIFT}{out}")
+
+
