c784401d3abcea06deb5794057c12d2280535a51
Franky <nikto8748@gmail.com>
2024-03-22 20:34:01 +0300

    Added attack to server.

--- 20240318/1/serv.py
+++ 20240318/1/serv.py
@@ -62,7 +62,18 @@ def serve(conn, addr):
                     field[(x, y)].monster = monster
                     conn.sendall((f"{replaced_monster_flag}").encode())
                 case "attack":
-
+                    if field[(player.x, player.y)].monster is None or \
+                            field[(player.x, player.y)].monster.name != inf[1]:
+                        conn.sendall((f"{False}").encode())
+                    else:
+                        damage = inf[2]
+                        if field[(player.x, player.y)].monster.hp > damage:
+                            field[(player.x, player.y)].monster.hp -= damage
+                            conn.sendall((f"{True} {damage} {field[(player.x, player.y)].monster.hp}").encode())
+                        else:
+                            damage = field[(player.x, player.y)].monster.hp
+                            field[(player.x, player.y)].monster = None
+                            conn.sendall((f"{True} {damage} {0}").encode())
                 case _:
                     pass
 
